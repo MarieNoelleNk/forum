56d4a7937a04d0cf83cf93a573a682d84e13bbcb
kinaya237 <kinaya237@gmail.com>
2019-09-22 15:52:48 +0200

    mise à jour du manager

--- app/Model/Manager.php
+++ app/Model/Manager.php
@@ -8,7 +8,8 @@ class Manager
 {
     protected function dbConnect()
     {
-        $database = new \PDO ('mysql:host=localhost;dbname=orcandie;charset=utf8', 'root', '');
+        $database = new \PDO ('mysql:host=db5000172777.hosting-data.io;dbname=dbs167667;charset=utf8', 'dbu248270',
+            'Orcandie78!');
         $database->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         return $database;
 
